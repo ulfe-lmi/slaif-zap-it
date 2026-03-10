53495b74631863f7466eafb38085d99a40411a74
jpers1 <30219753+jpers1@users.noreply.github.com>
2026-03-10 19:42:02 +0100

    Revert "Add 3-category ordinal Krippendorff alpha helper"

--- src/__init__.py
+++ src/__init__.py
@@ -42,7 +42,7 @@ from .batch import (
     segment_images,
     segment_video,
 )
-from .postprocessing import filter_by_area_bbox, krippendorff_alpha_ordinal, krippendorf_alfa
+from .postprocessing import filter_by_area_bbox
 
 __all__ = [
     "log_print",
@@ -58,6 +58,4 @@ __all__ = [
     "segment_images",
     "segment_video",
     "filter_by_area_bbox",
-    "krippendorff_alpha_ordinal",
-    "krippendorf_alfa",
 ]
